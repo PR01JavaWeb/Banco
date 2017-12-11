f303588fb74f884a6c2ebc5a2bcbd7d809c54187
Ruben <rubenjuradogordillo@gmail.com>
2017-12-11 16:38:16 +0100

    Prueba

--- pr01javaweb/src/java/controller/ClienteController.java
+++ pr01javaweb/src/java/controller/ClienteController.java
@@ -28,7 +28,7 @@ public class ClienteController extends HttpServlet {
      *
      * @param request servlet request
      * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
+
      * @throws IOException if an I/O error occurs
      */
     @Override
