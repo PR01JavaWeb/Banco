25549a4e945c1599a6c8122842ed3cba116882b0
Oscar <oscarsc995@gmail.com>
2017-12-11 16:25:00 +0100

    Primera version ficheros java

--- pr01javaweb/src/java/model/Conexion.java
+++ pr01javaweb/src/java/model/Conexion.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
 
 /**
  *
- * @author Oscar
+ * @author Banco
  */
 public class Conexion {
+    public String db="bd_banco";
+    public String url="jdbc:mysql://127.0.0.1/" +db;
+    public String user="root";
+    public String pass="";
+
+    public Conexion() {
+    }
     
+    public Connection conectar(){
+        Connection link=null;
+        
+        try {
+            //Usar la classe DriverManager para eso he de importar el Driver 
+            //a traves de un fichero jar
+            
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link=DriverManager.getConnection(this.url, this.user, this.pass);
+            
+            //System.out.println("Conexion establecida con exito");
+                    
+            JOptionPane.showMessageDialog(null, "Conexion correcta");      
+            
+            
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            JOptionPane.showMessageDialog(null, "Conexion erronea");
+            
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            JOptionPane.showMessageDialog(null, "Conexion erronea");
+        }
+        
+       return link;
+    }    
 }
